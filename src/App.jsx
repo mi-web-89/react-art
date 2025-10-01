f1037e464efd373f12316f1f46e198b5e09be92f
muhammad_ismail <mi272401@gmail.com>
2025-10-01 13:21:24 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from './components/auth.jsx';
+import Header from './components/header.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
